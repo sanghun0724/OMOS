e2badab759163b12c7fd51346dbdbf1cc55065b2
sanghun0724 <sanghun0724@naver.com>
2022-04-29 20:57:38 +0900

    delete dummy data

--- Omos/Presenter/UI/CoreTabs/ViewModels/HomeViewModel.swift
+++ Omos/Presenter/UI/CoreTabs/ViewModels/HomeViewModel.swift
@@ -50,8 +50,7 @@ class HomeViewModel: BaseViewModel {
                 self?.popuralLoading.onNext(false)
                 switch event {
                 case .success(let data):
-                let test = [ PopuralResponse(music: Music(albumImageURL: "https://i.scdn.co/image/ab67616d00001e020774e9bcd9d27d9cc64712ce", albumTitle: "Goodbye, grief.", artists: [Omos.Artist(artistID: "6evmYxFbDSIHilUaYC9MhL", artistName: "자우림")], musicID: "4gLkg28D1HCQkQemOceg4x", musicTitle: "이카루스"), nickname: "Ilish", recordID: 322, recordImageURL: "https://omos-image.s3.ap-northeast-2.amazonaws.com/record/1031649638831440.png", recordTitle: "자, 힘차게 날개를 펴고 날아보자", userID: 103), PopuralResponse(music: Music(albumImageURL: "https://i.scdn.co/image/ab67616d00001e02bf55580ea719aa52c2bbece7", albumTitle: "PROTOTYPE RESEARCH #0063", artists: [Artist(artistID: "31VffPWiL2AAwNIMODB9qZ", artistName: "김승민"), Artist(artistID: "2FgZrgTMX6Sk0VNcOsEPmm", artistName: "펀치")], musicID: "6eyVKpe2LGukVfC8wpd3D2", musicTitle: "One, Two (Feat. Punch)"), nickname: "재르민", recordID: 313, recordImageURL: "https://omos-image.s3.ap-northeast-2.amazonaws.com/record/1081649591110551.png", recordTitle: "내려가는 길", userID: 108), PopuralResponse(music: Music(albumImageURL: "https://i.scdn.co/image/ab67616d00001e02344da4e90fc0daf77f1b8d2b", albumTitle: "Great Wave", artists: [Artist(artistID: "4xgRWQOK2y3pGRFtmWNjyw", artistName: "신승훈")], musicID: "4L0l5DqXYIHoUdb7HvauOH", musicTitle: "Sorry"), nickname: "루시", recordID: 358, recordImageURL: "https://omos-image.s3.ap-northeast-2.amazonaws.com/record/301650987206168.png", recordTitle: "아직도 내 기억들이 따끔거리길", userID: 30) ]
-                    self?.currentPopuralRecord = test
+                    self?.currentPopuralRecord = data
                     self?.popuralRecord.onNext(data)
                 case .failure(let error):
                     self?.errorMessage.onNext(error.localizedDescription)
@@ -82,7 +81,6 @@ class HomeViewModel: BaseViewModel {
                 switch result {
                 case .success(let data):
                     self?.currentRecommentRecord = data
-                    self?.currentRecommentRecord = [RecommendDjResponse(nickname: "Ilish", profileURL: Optional("https://omos-image.s3.ap-northeast-2.amazonaws.com/profile/103.png"), userID: 103), RecommendDjResponse(nickname: "맹고", profileURL: Optional("https://omos-image.s3.ap-northeast-2.amazonaws.com/profile/120.png"), userID: 120), RecommendDjResponse(nickname: "지지니", profileURL: nil, userID: 123)]
                     self?.recommendRecord.onNext(data)
                 case .failure(let error):
                     self?.errorMessage.onNext(error.localizedDescription)
